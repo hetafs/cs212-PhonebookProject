765329f75515b959dc24f709d506e5c42f730fde
lamasox20 <147297859+lamasox20@users.noreply.github.com>
2023-10-16 22:48:49 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -29,21 +29,45 @@ public class LinkedList<T extends Comparable<T>> {
     public T retrieve () {
             return current.data;
     }
-    public void update (T val) {
-            current.data = val;
+    public void update (T d) {
+            current.data = d;
     }
-    
-  
-    public boolean insertSort (T val) {
+
+    /* AnotherOOOpation
+    public void sort_Aadd(T d){
+        Node<T> Contact_= new Node<T>(d);
+        //empty List
+        
+        if(head==null){
+            head=current=Contact_;
+        }
+        else{
+            //if the Node is smaller then the head we will insert it at First As head  
+            if(((Contact)Contact_.data).compareTo((Contact)head.data)<0){
+                Contact_.next=head;
+                head=Contact_;}
+            
+            
+            else{
+                Node<T> pointer=head;
+                Node<T> pointer_2=null;
+                while(pointer!=null&&(((Contact)pointer.data).compareTo((Contact)Contact_.data)<=0)){
+                    pointer_2=pointer;
+                    pointer=pointer.next;}
+                //now we add it at middlel or at last 
+                pointer_2.next=Contact_;
+                Contact_.next=pointer;}       
+        }}
+    public boolean insertSort (T d) {
            
             Node<T> tmp;
             if (empty()) {
-                    current = head = new Node (val);
+                    current = head = new Node (d);
             }
             else {
-                if ( head.getData().compareTo(val) >0)
+                if ( head.getData().compareTo(d) >0)
                 {
-                    tmp = new Node(val);
+                    tmp = new Node(d);
                     tmp.setNext(head);
                     head = tmp;
                 }
@@ -52,12 +76,12 @@ public class LinkedList<T extends Comparable<T>> {
                     Node<T> prev = null;
                     current = head;
                     
-                    while (( current != null ) && (current.getData().compareTo(val) <= 0))
+                    while (( current != null ) && (current.getData().compareTo(d) <= 0))
                     {
                         prev = current;
                         current = current.getNext();
                     }
-                    tmp = new Node (val);
+                    tmp = new Node (d);
                     if ( current != null)
                     {
                         tmp.next = current;
@@ -71,26 +95,30 @@ public class LinkedList<T extends Comparable<T>> {
             size++;
             return true;
     }
+	
+	
 
-    public boolean search (T val) {
+    public boolean search (T d) {
 	    Node<T> serNode = head;
-
-	    while (serNode != null && serNode.getData().compareTo(val) != 0) {
+	    //we move through list until the end or a match =d is found
+	    while (serNode != null && serNode.getData().compareTo(d) != 0) {
 	    	serNode = serNode.getNext();
 	    }
+	    // if it's not null that's means that we found it we will return ture
 
 	    if (serNode != null) {
 	    	current=serNode ;
 	        return true;
 	    }
+	    // we serch throw the whole list it is not exsist return false
 
 	    return false;
 	}  
     
     
-    public T remove (T val) {
+    public T remove (T d) {
             
-        if (!search (val) )
+        if (!search (d) )
          return null;
 
         T data = current.getData();
@@ -113,14 +141,16 @@ public class LinkedList<T extends Comparable<T>> {
         return data;    
     }
 
-        public void printAll ()
-        {
-            Node  current = head;
-            while ( current != null)
+        public void display(){
+        
+            Node<T> Cueent_node= head;
+            //we will let the  move until the end of list 
+            while ( Cueent_node!= null)
             {
-                System.out.print(current.data + "    ");
-                current= current .next;
+                System.out.print(Cueent_node.data +" ");
+                Cueent_node = Cueent_node.next;
             }
             System.out.println();
         }
+}
 }
